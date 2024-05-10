74f4d7f70f35fa56066663d4a0d3af4eca47497a
Etienne Bertin <etienne.bertin@ensta-paristech.fr>
2024-05-10 15:37:01 +0200

    added untracked text file for ip address

--- server.py
+++ server.py
@@ -34,4 +34,6 @@ def getBattleInfoFromForm ():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    ip_file = open ('ip.txt')
+    ip = ip_file.readline ()
+    app.run(debug=True, host=ip)
